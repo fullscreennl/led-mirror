25672375f4d6181c8aab81891630bab3b61f9758
johantenbroeke <johan@fullscreen.nl>
2014-12-19 19:50:23 +0100

    4 spaces tabwidth

--- software/main.c
+++ software/main.c
@@ -3,46 +3,46 @@
 
 int quantize(int level)
 {
-   int output_pixel;
-   if(level < 50){
-      output_pixel = 0;
-   }else if(level < 150){
-      output_pixel = 1;
-   }else if(level < 220){
-      output_pixel = 2;
-   }else{
-      output_pixel = 3;
-   }
-   return output_pixel;
+    int output_pixel;
+    if(level < 50){
+        output_pixel = 0;
+    }else if(level < 150){
+        output_pixel = 1;
+    }else if(level < 220){
+        output_pixel = 2;
+    }else{
+        output_pixel = 3;
+    }
+    return output_pixel;
 }
 
 void test_ImageDraw(int framecounter){
 
     int frc = framecounter%(1900/32);
-	
+
     int offset = frc * 32;
     int xoffset = frc%32-15; 
 
     int image[20] = { 15 + offset + xoffset, 
-                      16 + offset + xoffset, 
-                      47 + offset + xoffset, 
-                      48 + offset + xoffset, 
-                      77 + offset + xoffset,
-                      78 + offset + xoffset,
-                      79 + offset + xoffset,
-                      80 + offset + xoffset,
-                      81 + offset + xoffset,
-                      82 + offset + xoffset, 
-                      109 + offset + xoffset,
-                      110 + offset + xoffset,
-                      111 + offset + xoffset,
-                      112 + offset + xoffset,
-                      113 + offset + xoffset,
-                      114 + offset + xoffset, 
-                      143 + offset + xoffset,
-                      144 + offset + xoffset,
-                      175 + offset + xoffset,
-                      176 + offset + xoffset };
+                    16 + offset + xoffset, 
+                    47 + offset + xoffset, 
+                    48 + offset + xoffset, 
+                    77 + offset + xoffset,
+                    78 + offset + xoffset,
+                    79 + offset + xoffset,
+                    80 + offset + xoffset,
+                    81 + offset + xoffset,
+                    82 + offset + xoffset, 
+                    109 + offset + xoffset,
+                    110 + offset + xoffset,
+                    111 + offset + xoffset,
+                    112 + offset + xoffset,
+                    113 + offset + xoffset,
+                    114 + offset + xoffset, 
+                    143 + offset + xoffset,
+                    144 + offset + xoffset,
+                    175 + offset + xoffset,
+                    176 + offset + xoffset };
 
     displayImage(image,NELEMS(image));
 

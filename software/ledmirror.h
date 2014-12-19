461d76fd6d6512e91deb93fa09aa67df7ba19257
johantenbroeke <johan@fullscreen.nl>
2014-12-19 11:44:10 +0100

    added header file for ledmirror

---
+++ software/ledmirror.h
@@ -0,0 +1,7 @@
+#ifndef LEDMIRROR_H
+#define LEDMIRROR_H
+
+
+int ledmirror_run();
+
+#endif
---
+++ software/main.c
@@ -0,0 +1,8 @@
+#include "ledmirror.h"
+
+int main(int argc, const char **argv)
+{
+    int exitcode = ledmirror_run();
+    return exitcode;
+}
+

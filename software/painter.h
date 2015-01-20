5747882ff066008dc1055ddc94d0b3719361a6ac
johantenbroeke <johan@fullscreen.nl>
2015-01-20 10:43:47 +0000

    comments to the apps header files

--- software/painter.h
+++ software/painter.h
@@ -1,6 +1,9 @@
 #ifndef PAINTER_H
 #define PAINTER_H
 
+//Implementation of the ledmirror protocol, so the video events can be forwarded
+//if this app is selected.
+
 int painter_quantize(int level);
 void painter_videoFrameDidRender(MMAL_BUFFER_HEADER_T *buffer, int framecounter);
 void painter_videoFrameWillRender(int framecounter);
